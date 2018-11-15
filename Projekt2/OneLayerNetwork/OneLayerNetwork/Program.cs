4449928d85c54b55f2a9baad84a80e48783dc4be
MarcinDamek <marcind70@gmail.com>
2018-11-15 22:37:15 +0100

    Implementacja Adeline

---
+++ Projekt2/OneLayerNetwork/OneLayerNetwork/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace OneLayerNetwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var learningRate = 0.1;
+            Console.WriteLine("Learning rate: " + learningRate);
+            var adaline = new Adaline(learningRate);
+            adaline.Learn();
+            adaline.Test();
+
+        }
+    }
+}
